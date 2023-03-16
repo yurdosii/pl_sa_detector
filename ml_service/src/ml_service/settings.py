0ca3e19120d3775dfa2c9795b5df2aa3513c8c0f
Yurii Kosiv <yurdosii.ksv@gmail.com>
2023-03-16 11:23:51 +0200

    feat: update ML service

--- ml_service/src/ml_service/settings.py
+++ ml_service/src/ml_service/settings.py
@@ -1,7 +1,7 @@
 from sklearn.feature_extraction.text import BaseEstimator
 
 # ML models path
-PL_MODEL_PATH = "pl_model.pkl"
-SA_MODEL_PATH = "sa_model.pkl"
+PL_MODEL_PATH: str = "pl_model.pkl"
+SA_MODEL_PATH: str = "sa_model.pkl"
 
 ML_MODELS: dict[str, BaseEstimator] = {}
